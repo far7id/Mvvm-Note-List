a3552136eb63e51c939ce7c95afc5a9ea2198b92
farid <faridmhn77@gmail.com>
2019-07-22 13:58:17 +0430

    Initial commit

---
+++ app/src/main/java/com/example/mvvmrecycler/NoteAdapter.java
@@ -0,0 +1,93 @@
+package com.example.mvvmrecycler;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mvvmrecycler.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder>
+{
+    private List<Note> notes = new ArrayList<>();
+    private OnItemclickListener listener;
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent,false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position)
+    {
+        Note currentNote = notes.get(position);
+        holder.txtTitle.setText(currentNote.getTitle());
+        holder.txtDescription.setText(currentNote.getDescription());
+        holder.txtPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes)
+    {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public Note getNoteAt(int position)
+    {
+        return notes.get(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder
+    {
+        private TextView txtTitle;
+        private TextView txtDescription;
+        private TextView txtPriority;
+
+        public NoteHolder(View itemView)
+        {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.txt_view_title);
+            txtDescription = itemView.findViewById(R.id.txt_view_description);
+            txtPriority = itemView.findViewById(R.id.txt_view_priority);
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION)
+                    {
+                        listener.onItemClick(notes.get(position));
+                    }
+                }
+            });
+
+        }
+    }
+
+    public interface OnItemclickListener
+    {
+        void onItemClick(Note note);
+    }
+
+    public void setOnItemClickListener(OnItemclickListener listener)
+    {
+        this.listener = listener;
+    }
+}
---
+++ app/src/main/java/com/example/mvvmrecycler/view/AddEditNoteActivity.java
@@ -0,0 +1,105 @@
+package com.example.mvvmrecycler.view;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.example.mvvmrecycler.R;
+
+public class AddEditNoteActivity extends AppCompatActivity
+{
+    public static final String EXTRA_ID = "com.example.mvvmrecycler.EXTRA_ID";
+    public static final String EXTRA_TITLE = "com.example.mvvmrecycler.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION = "com.example.mvvmrecycler.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY = "com.example.mvvmrecycler.EXTRA_PRIORITY";
+
+    private EditText edittxt_title;
+    private EditText edittxt_description;
+    private NumberPicker number_picker_priority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        edittxt_title = findViewById(R.id.edittxt_title_xml);
+        edittxt_description = findViewById(R.id.edittxt_description_xml);
+        number_picker_priority = findViewById(R.id.number_picker_priority_xml);
+
+        number_picker_priority.setMinValue(1);
+        number_picker_priority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_ID))
+        {
+            setTitle("Edit Note");
+            edittxt_title.setText(intent.getStringExtra(EXTRA_TITLE));
+            edittxt_description.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            number_picker_priority.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+        }
+
+        else
+        {
+            setTitle("Add Note");
+        }
+    }
+
+    private void saveNote()
+    {
+        String title = edittxt_title.getText().toString();
+        String description = edittxt_description.getText().toString();
+        int priority = number_picker_priority.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty())
+        {
+            Toast.makeText(this, "please insert title and description", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1)
+        {
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.save_note:
+                saveNote();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+}
